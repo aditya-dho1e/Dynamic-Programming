f47612321fd8b75fcd38a75a09801cc92ce1cba5
Aditya Dhole <106442327+aditya-dho1e@users.noreply.github.com>
2023-04-09 11:15:49 +0530

    Create min-cost-climbing-stairs.cpp

---
+++ min-cost-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Input: cost = [10,15,20]
+Output: 15
+
+class Solution {
+public:
+    int f(int i, vector<int>&arr,vector<int>&dp){
+        if(i<0)return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=min(arr[i]+f(i-1,arr,dp),arr[i]+f(i-2,arr,dp));
+    }
+    int minCostClimbingStairs(vector<int>&arr) {
+        int n=arr.size();
+        vector<int>dp(n+1,-1);
+
+        int mini=f(n-1,arr,dp);
+
+        dp.assign(n+1,-1);
+        
+        return min(mini,f(n-2,arr,dp));
+    }
+};
